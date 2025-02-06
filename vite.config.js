29d03bfc3a00ec35c4883e804fe78a02720b8813
Alexis Thierry <alexist103@gmail.com>
2025-02-06 20:21:49 +0200

    ajout viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import restart from 'vite-plugin-restart'
-
 export default {
     root: '/', // Sources files (typically where index.html is)
     publicDir: '/public/', // Path from "root" to static assets (files that are served as they are)
@@ -14,8 +12,4 @@ export default {
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
-    plugins:
-    [
-        restart({ restart: [ '/public/**', ] }) // Restart server on static file change
-    ],
 }
